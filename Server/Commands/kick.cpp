a2c14ca6be63607d063f52f8cd844e9cc12e3e9f
Leo Moreno <leon.moreno@pm.me>
2023-04-17 16:33:38 -0400

    avances kick

--- Server/Commands/kick.cpp
+++ Server/Commands/kick.cpp
@@ -15,6 +15,9 @@
 std::string Server::kick(Client * client, Message & msg) {
     std::vector<std::string> params;
 
+    std::cout << "Params = " << msg.params.size() << std::endl;
+    std::cout << "Params_01 = " << msg.params[1] << std::endl;
+
     if (msg.params.size() < 2) {
         params.push_back(client->getNickname());
         params.push_back(client->getMode());
@@ -30,27 +33,28 @@ std::string Server::kick(Client * client, Message & msg) {
         params.push_back(msg.params[0]);
         return (generateResponse("ft_irc", "403", params, "No such channel"));
     }
+    if (this->getClientByName(msg.params[1]) == false) {
+        params.push_back(msg.params[0]);
+        return (generateResponse("ft_irc", "442", params, "User not on that channel"));
+    }
 
-
-//   std::vector<Client *>::iterator it;
-//   std::cout << "Server had: " << this->_userbase.size() << " users" << std::endl;
-//   for (it = this->_userbase.begin(); it != this->_userbase.end(); ) {
-//     if ((*it)->getNickname() == client->getNickname()) {
-//       // std::cout << "found the client!" << std::endl;
-//       close(client->getSocket());
-//       delete (*it);
-//       it = this->_userbase.erase(it);
-//     } else {
-//       ++it;
-//     }
-//   }
-  // std::cout << "Server now has: " << this->_userbase.size() << " users" << std::endl;
+    std::cout << "Server had: " << this->_userbase.size() << " users" << std::endl;
+    std::vector<Client *>::iterator it;
+    Client *tmp = this->getClientByName(msg.params[1]);
+    if (tmp != nullptr)
+        std::cout << "Client_NICK = " << tmp->getNickname() << std::endl;
+    for (it = this->_userbase.begin(); it != this->_userbase.end(); ) {
+        if ((*it)->getNickname() == tmp->getNickname()) {
+            std::cout << tmp->getNickname() << " user kick !!" << std::endl;
+            close(tmp->getSocket());
+            delete (*it);
+            it = this->_userbase.erase(it);
+            
+        } else {
+            ++it;
+        }
+    }
+    std::cout << "Server now has: " << this->_userbase.size() << " users" << std::endl;
   
-  // std::vector<std::string> params;
-  // params.push_back(client->getNickname());
-  // params.push_back(message.command);
-  // if (message.params.size() == 1)
-  //   params.push_back(message.params[0]);
-  // std::string response = generateResponse("ft_irc", "", params, "");
   return ("");
 }
